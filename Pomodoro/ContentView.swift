53caba1f41bc88ee9099dd07a281a7ef457d1d84
Rajesh Khuntia <rajeshk001@icloud.com>
2023-09-06 06:17:26 +0530

    Initial Commit

---
+++ Pomodoro/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by Rajesh Khuntia on 03/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack{
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/){ Image(systemName: "music.note").foregroundColor(.white)}
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/){ Image(systemName: "slider.horizontal.3").foregroundColor(.white)}
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/){ Image(systemName: "rectangle.landscape.rotate").foregroundColor(.white)}
+                Spacer()
+            }
+            VStack{
+                Text("Stay focused")
+                Text("24.53").font(.title).padding(5)
+                Text("Reading")
+            }
+            .frame(width: 300, height:300)
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 500)
+//                    .stroke(Color.white, lineWidth: 3)
+//            )
+            .border(Color.black)
+            .padding(50)
+            HStack{
+                Spacer()
+                Image(systemName: "play.fill")
+                    .padding()
+                    .background(.yellow)
+                    .cornerRadius(200)
+                Spacer()
+                Image(systemName: "square.fill")
+                    .foregroundColor(.yellow)
+                    .padding()
+                    .background(.gray)
+                    .cornerRadius(200)
+                Spacer()
+            }
+            
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Rajesh Khuntia on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
